988c406c71292ab102e75135c692eee0eb77cdd6
eatplaynap <eatplaynap329@gmail.com>
2021-06-22 14:25:14 +0900

    7章終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_074342) do
+ActiveRecord::Schema.define(version: 2021_06_22_050928) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
+    t.string "picture"
   end
 
 end
